688e8b613952cc4f3942bd04cc5305093b1ee6fc
vizer159951 <106464355+vizer159951@users.noreply.github.com>
2025-02-06 18:08:21 +0300

    Add files via upload

---
+++ ESP32_CAM/ESP32_CAM.ino
@@ -0,0 +1,64 @@
+#include "OV2640.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+
+#include "SimStreamer.h"
+#include "OV2640Streamer.h"
+#include "CRtspSession.h"
+
+OV2640 cam;
+WiFiServer rtspServer(8554);
+
+unsigned long lastimage;
+int msecPerFrame = 1000;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {;}
+  cam.init(esp32cam_aithinker_config);
+  lastimage = millis();
+
+  IPAddress ip;
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("RT-GPON-6AC8", "D7g3d6j764");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  ip = WiFi.localIP();
+  Serial.println(F("WiFi connected"));
+  Serial.println("");
+  Serial.println(ip);
+  rtspServer.begin();
+}
+
+CStreamer *streamer;
+CRtspSession *session;
+WiFiClient client;
+
+void loop() {
+  if (session) {
+    if (millis() > lastimage + msecPerFrame) {
+      session->handleRequests(0);
+
+      session->broadcastCurrentFrame(millis());
+      Serial.println("Delta time: " + (String)(millis() - lastimage));
+
+      lastimage = millis();
+    }
+
+    if (session->m_stopped) {
+      delete session;
+      delete streamer;
+      session = NULL;
+      streamer = NULL;
+    }
+  } else {
+    client = rtspServer.accept();
+    if (client) {
+      streamer = new OV2640Streamer(&client, cam);
+      session = new CRtspSession(&client, streamer);
+    }
+  }
+}
